38ec09b50f8940f40227040313108c07d4005e10
colson <dud3722000@naver.com>
2020-08-12 12:19:47 +0900

    .

--- Simulation/paper_list.m
+++ Simulation/paper_list.m
@@ -1,2 +1,4 @@
 % (1) Distributed Kalman filter for cooperative localization with
+% (2) A novel cooperative localization algorithm using enhanced particle
+% filter technique in maritime search and rescue wireless sensor network
 % integrated measurement (DKFCL)
