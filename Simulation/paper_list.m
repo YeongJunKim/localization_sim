5fa96be25c273392c2ca400002f9f87cea829ae2
colson <dud3722000@naver.com>
2020-09-21 12:40:08 +0900

    restore deleted file

---
+++ Simulation/paper_list.m
@@ -0,0 +1,5 @@
+% (1) Distributed Kalman filter for cooperative localization with
+% (2) A novel cooperative localization algorithm using enhanced particle
+% (3) Distributed Multirobot Localization
+% filter technique in maritime search and rescue wireless sensor network
+% integrated measurement (DKFCL)
